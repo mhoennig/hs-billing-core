6269ee050259cb873130b3f5a761aca1ae743724
Michael Hoennig <michael@hoennig.de>
2020-12-07 10:43:29 +0100

    reference to pecial git branch 'archunit-rule-validation' in ArchitectureTest

--- app/src/test/kotlin/org/hostsharing/hsadmin/billing/core/ArchitectureTest.kt
+++ app/src/test/kotlin/org/hostsharing/hsadmin/billing/core/ArchitectureTest.kt
@@ -14,32 +14,48 @@ internal val ALL_PACKAGES = arrayOf(
     "${ROOT_PACKAGE}.writer",
     "${ROOT_PACKAGE}.lib")
 
+/**
+    Verification of some architecture rules, e.g. package depenencies.
+
+    <p>There is a special git branch 'archunit-rule-validation' which deliberately breaks each rule.
+    To verify changed arch rules, you can rebase that branch to the master branch
+    and check if the rules are correct by running `./gradlew test`.</p>
+
+    <p>See also 'resources/archunit.properties' for configuration.</p>
+
+    <p>Please also add such "rule-tests" to that branch when you change these rules.</p>
+ */
 @AnalyzeClasses(packages = ["${ROOT_PACKAGE}.."])
 internal class ArchitectureTest {
     @ArchTest
-    val `will not have cycles between packages`: ArchRule =
+    val `will not have cycles between packages`: ArchRule = verify(
         slices().matching("${ROOT_PACKAGE}.(*)..")
             .should().beFreeOfCycles()
+    )
 
     @ArchTest
-    val `package 'lib' will not use any other package`: ArchRule =
+    val `package 'lib' will not use any other package`: ArchRule = verify(
         noClasses()
             .that().resideInAPackage("..lib..")
             .should().dependOnClassesThat().resideInAnyPackage(
                 *exceptPackageEndsWithAnyOf(".lib")
             )
+    )
 
     @ArchTest
-    val `package 'domain' will not use any other package except 'lib'`: ArchRule =
+    val `package 'domain' will not use any other package except 'lib'`: ArchRule = verify(
         noClasses()
             .that().resideInAPackage("..domain..")
             .should().dependOnClassesThat().resideInAnyPackage(
                 *exceptPackageEndsWithAnyOf(".domain", ".lib")
             )
+    )
 
     private fun exceptPackageEndsWithAnyOf(vararg packageEndings: String): Array<String> =
         ALL_PACKAGES.filter{
             !packageEndings.any{ ending -> it.endsWith(ending)}
         }.toTypedArray()
-
 }
+
+// in the git branch 'archunit-rule-validation', this function freezes the results
+fun verify(archRule: ArchRule): ArchRule = archRule
