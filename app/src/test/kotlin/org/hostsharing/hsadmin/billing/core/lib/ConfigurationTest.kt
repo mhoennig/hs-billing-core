0bca09ab79d7c8760f020a5c469d4359d8af8100
Michael Hoennig <michael@hoennig.de>
2020-12-20 11:02:41 +0100

    improves test coverage for Configuration

---
+++ app/src/test/kotlin/org/hostsharing/hsadmin/billing/core/lib/ConfigurationTest.kt
@@ -0,0 +1,34 @@
+package org.hostsharing.hsadmin.billing.core.lib
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class ConfigurationTest {
+
+    @Test
+    fun `getting property templatesDirectory throws exception if not overridden`() {
+        val given = object : Configuration{}
+        val actual = assertThrows<IllegalStateException> {
+            given.templatesDirectory
+        }
+        assertThat( actual.message).isEqualTo("not configured")
+    }
+
+    @Test
+    fun `getting property outputDirectory throws exception if not overridden`() {
+        val given = object : Configuration{}
+        val actual = assertThrows<IllegalStateException> {
+            given.outputDirectory
+        }
+        assertThat( actual.message).isEqualTo("not configured")
+    }
+
+    @Test
+    fun `default for property domesticCountryCode is 'DE'`() {
+        val given = object : Configuration{}
+        val actual = given.domesticCountryCode
+        assertThat( actual).isEqualTo("DE")
+    }
+}
